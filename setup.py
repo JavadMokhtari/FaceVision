1b400f014136e11d2911b3db927c50c931898da3
j.mokhtari <j.mokhtari@itgroup.org>
2024-07-20 17:50:21 +0330

    Updated setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email='j.mokhtari@itgroup.org',
     packages=find_packages(),
     python_requires='>=3.12',
-    # package_data={'facevision': ['assets/pretrained/*', 'sixdrepnet/*']},
+    # package_data={'facevision': ['assets/weights/*', 'sixdrepnet/*']},
     include_package_data=True,
     # install_requires=['numpy==1.26.4', 'pillow==10.3.0', 'matplotlib==3.9.0', 'scipy==1.13.1',
     #                   'opencv-python==4.10.0.84', 'mediapipe==0.10.14', 'torch==2.3.1', 'torchvision==0.18.1']
