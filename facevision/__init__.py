7787a37c59e49a1730ac3af71d75adc8cff71d6a
j.mokhtari <j.mokhtari@itgroup.org>
2024-07-20 17:50:42 +0330

    Updated __init__.py

--- facevision/__init__.py
+++ facevision/__init__.py
@@ -1,7 +1,6 @@
-import analyzer
-
-
-__description__ = """Face Vision:\n
-A powerful tool for comprehensive face image analysis, capable of measuring face quality."""
+__description__ = """
+    Face Vision:\n
+    A powerful tool for comprehensive face image analysis, capable of measuring face quality.
+    """
 __author__ = "Javad Mokhtari"
 __version__ = "0.0.0"
